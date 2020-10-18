1f2449506b10c5dda831c543760d420bad0c0a06
barnabasivan <barnabas.ivan07@gmail.com>
2020-10-18 14:47:15 +0200

    001 megoldva

--- 001/main.py
+++ 001/main.py
@@ -1,11 +1,11 @@
 """
 Kerj be ket egesz szamot, ird ki a nagyobbikat.
 """
-elso=int(input("Adj meg egy számot: "))
-masodik=int(input("Adj meg még egy számot: "))
-if elso>masodik:
-    print(elso)
+x=int(input("Adj meg egy számot: "))
+y=int(input("Adj meg még egy számot: "))
+if x>y:
+    print(x)
 else:
-    print(masodik)
+    print(y)
     
 
