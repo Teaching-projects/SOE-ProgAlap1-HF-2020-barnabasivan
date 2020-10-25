006b01637e6653c46595f3421df12d4e8a89d50b
barnabasivan <barnabas.ivan07@gmail.com>
2020-10-25 19:38:52 +0100

    7kesz

--- 007/main.py
+++ 007/main.py
@@ -1,1 +1,18 @@
+noszamla = 0
+honap = 0
+egyenleg = 0
 
+while honap != 12:
+    pmozgas = int(int(input()))
+    egyenleg += pmozgas
+    noszamla += pmozgas
+    if honap != 0:
+        egyenleg -= 2000
+    if egyenleg > 0:
+        egyenleg += int(egyenleg * 0.05)
+    else:
+        egyenleg += int(egyenleg * 0.1)
+    honap += 1
+    
+print(int(egyenleg))
+print(noszamla)
