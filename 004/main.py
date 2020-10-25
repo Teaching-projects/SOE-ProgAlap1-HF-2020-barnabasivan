6397c2c761322f629698b422f387e4f3433dca1d
barnabasivan <barnabas.ivan07@gmail.com>
2020-10-25 15:41:54 +0100

    4kesz

--- 004/main.py
+++ 004/main.py
@@ -9,3 +9,24 @@ Kimenet:
 3
 54
 """
+elso = int(input())
+masodik = int(input())
+if (elso>0 and masodik>0):
+    if (elso > masodik):
+        nagyobb = elso
+        kisebb = masodik
+    else:
+        nagyobb = masodik
+        kisebb = elso
+
+    index = 1
+    szam = kisebb
+    while index == 1:
+        if (kisebb % szam == 0 and nagyobb % szam == 0):
+            lko = szam
+            index = 0
+        szam -= 1
+    print(lko)
+
+print(elso * masodik // lko)
+
