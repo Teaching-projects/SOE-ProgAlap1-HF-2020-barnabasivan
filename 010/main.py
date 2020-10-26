4d66b684ae2ca8c3bf8d9641fafe436ce202a3bc
barnabasivan <barnabas.ivan07@gmail.com>
2020-10-26 13:05:54 +0100

    zh

--- 010/main.py
+++ 010/main.py
@@ -1,13 +1,26 @@
+percek = []
+sms = []
 
+for honap in range(12):
+    percek.append(int(input()))
+    sms.append(int(input()))
 
+havidíj = int(input())
+percdíj = int(input())
+smsdíj = int(input())
 
+szamla = []
+total = 0
 
-szamla=[]
+for honap in range(12):
+    mennyilenne=percdíj*percek[honap] + smsdíj * sms[honap]
+    if mennyilenne > havidíj:
+        szamla.append(mennyilenne)
+    else:
+        szamla.append(havidíj)
 
-
-
+for sz in szamla:
+    total+=sz
 
 print(szamla)
 print(total)
-
-
